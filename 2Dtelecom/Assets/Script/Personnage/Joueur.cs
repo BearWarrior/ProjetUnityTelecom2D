c5985ec3bb7db591f248fe68d8f66b161e9b6174
BearWarrior <thgallais@gmail.com>
2015-09-20 18:19:18 +0200

    Premier Commit
    Premier Commit

---
+++ 2Dtelecom/Assets/Script/Personnage/Joueur.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Joueur : Personnage
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        pointDeVieMax = 10;
+        pointDeVie = pointDeVieMax - 5;
+
+        inventaire = new List<Objet>();
+
+        Ration res = new Ration();
+        inventaire.Add(res);
+    }
+	
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKey(KeyCode.P))
+        {
+            if(inventaire[0] is Consommable)
+            {
+                Debug.Log("consommable");
+                if(UtiliserConsommable((inventaire[0] as Consommable)))
+                {
+                    inventaire.RemoveAt(0);
+                }
+            }
+            else
+            {
+                Debug.Log("pas consommable");
+            }
+        }
+	}
+
+
+    bool UtiliserConsommable(Consommable c)
+    {
+        //Si on a réussi a soigner on renvoie true, sinon false
+        //Au retour, si true : on détruit l'objet, si false : on le laisse
+        if(Soigner(c.getPointDeVie()))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
---
+++ 2Dtelecom/Assets/Script/Personnage/Personnage.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Personnage : MonoBehaviour
+{
+    protected string nom;
+    protected float pointDeVie;
+    protected float pointDeVieMax;
+    //private float endurance;
+
+
+    //Inventaire du personnage
+    protected List<Objet> inventaire;
+   // protected List<ObjetDeQuete> inventaireDeQuete;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+
+    //Fonction appelée lors d'une prise de dégats
+    public void Hit(float damage)
+    {
+        pointDeVie -= damage;
+        if(pointDeVie <= 0)
+        {
+            Die();
+        }
+    }
+
+
+    //Fonction appelée lors de l'utilisation d'un consommable rendant des points de vie
+    //Si le soin a fonctionné (il n'avait pas toute sa vie) alors on renvoit true, sinon (ça sert à rien de soin) on renvoie false
+    public bool Soigner(float soin)
+    {
+        if(pointDeVie == pointDeVieMax)
+        {
+            return false;
+        }
+        else
+        {
+            pointDeVie += soin;
+            if (pointDeVie > pointDeVieMax)
+            {
+                pointDeVie = pointDeVieMax;
+            }
+            return true;
+        }
+        
+    }
+
+
+    //Fonction appelée lors de la mort du personnage
+    public void Die()
+    {
+
+    }
+
+    //Accesseur :  aka GetteurSetteur
+    public float getPointDeVie() { return pointDeVie; }
+    public string getNom() { return nom; }
+}
